3b468f78c41bfef7527621d13de06b31bdfad4f4
Patcharaphorn Lertvirul <kukkik.kpl@gmail.com>
2020-02-28 20:38:25 +0700

    sessions framework

--- catalog/views.py
+++ catalog/views.py
@@ -22,6 +22,10 @@ def index(request):
 
     num_books_hunger = Book.objects.filter(title__contains='Hunger').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -29,6 +33,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres_fantasy': num_genres_fantasy,
         'num_books_hunger': num_books_hunger,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
